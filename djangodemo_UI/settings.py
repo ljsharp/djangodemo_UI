b5fe97d1789a07f8a4161ea76556c1704d761ddb
Leslie Joe <leslie.kojo.okyere@outlook.com>
2020-11-03 17:11:44 +0000

    Upadte

--- djangodemo_UI/settings.py
+++ djangodemo_UI/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'djangodemo_UI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'signbankdb',
-        'USER': 'signbank_user',
-        'PASSWORD': '@fricaSL2020',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'signbankdb',
+#         'USER': 'signbank_user',
+#         'PASSWORD': '@fricaSL2020',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
